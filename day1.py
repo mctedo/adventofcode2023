f018d231a35e49d5d047e46e1fc230a631355881
mctedo <git@mctedo.co.uk>
2023-12-02 21:06:30 +0000

    Day 1git config --global user.name

---
+++ day1.py
@@ -0,0 +1,19 @@
+import re
+
+# Part 1
+input = open("day1.txt").readlines()
+
+total = 0
+
+for i in input:
+
+    # part 1
+    num = re.findall(r'\d', i)
+    first = num[0]
+    last = num[-1]
+    value = (int)(str(first) + str(last))
+    total += value
+    print("{} {} {}".format(first, last, value))
+
+print(total)
+
