3637725de97260fd70d16c7ea540808d41f710e8
mctedo <github@mctedo.co.uk>
2023-12-03 20:47:56 +0000

    Day 3 incomplete going badly

--- day3_part1.py
+++ day3_part1.py
@@ -1,5 +1,9 @@
 
-lines = open("day3_sample.txt").readlines()
+input = open("day3_sample.txt").read()
+lines = input.split('\\n')
+
+symbols = { s: [] for s in input if s not in '0123456789.\n' }
+symbolLocations = { (r,c): [] for r in range(len(lines[0])) for c in range(len(lines)) if input[r][c] in symbols }
 
 sumPartNumbers = 0
 
@@ -20,7 +24,18 @@ for l in range(len(lines)):
         if strNumber != '':
             number = (int)(strNumber)
             print(number)
-            sumPartNumbers += number
+
+            # Now check if the number is surrounded by any symbol
+            valid = False
+
+            edge = {(r, c) for r in (l-1, l, l+1)
+                       for c in range(startsAt-1, endsAt+1)}
+            
+            for o in edge:
+                if o in symbolLocations:
+                    sumPartNumbers += number
+                    print("Matched {}".format(number))
+                    break
 
 print("Sum Part Numbers: {}".format(sumPartNumbers))
 
