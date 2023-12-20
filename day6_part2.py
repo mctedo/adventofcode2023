092fb692db8315b2076f0de3d6999db61cb84ee8
mctedo <github@mctedo.co.uk>
2023-12-20 15:28:46 +0000

    Day 6 Part 2

---
+++ day6_part2.py
@@ -0,0 +1,32 @@
+import re
+from functools import reduce
+
+def extract_integers_from_string(string):
+    integers = re.findall(r'\d+', string)
+    return [num for num in integers]
+
+data = open("day6_puzzle.txt").readlines()
+times = extract_integers_from_string(data[0])
+distances = extract_integers_from_string(data[1])
+
+# Concatenate all values of data and cast it to an integer
+times = [int(''.join(times))]
+distances = [int(''.join(distances))]
+
+wins = []
+for i in range(len(times)):
+    print(f"Time: {times[i]}, Distance: {distances[i]}")
+
+    win = 0
+    for j in range(times[i]):
+        distance = (times[i] - j) * j
+        if distance > distances[i]:
+            win += 1
+
+    wins.append(win)
+
+# Multiply all values of wins together
+result = reduce(lambda x, y: x * y, wins)
+print("Result:", result)
+    
+
