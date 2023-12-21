5385b4c954e95559a295bb8081beb4b47c154437
mctedo <github@mctedo.co.uk>
2023-12-21 19:47:03 +0000

    Day 8 Part 2 after a reddit hint that it's a lcm problem

--- day8_part2.py
+++ day8_part2.py
@@ -1,3 +1,4 @@
+import math
 input = open("day8_puzzle.txt").readlines()
 instructions = input[0].strip()
 
@@ -9,9 +10,9 @@ keys_with_a_as_last_character = [key for key in map.keys() if key.endswith('A')]
 current_instructions = keys_with_a_as_last_character
 current_index = 0
 moves = 0
+lcm = [None] * len(current_instructions)  # Initialize lcm list with None values
 
-
-while not all(instruction.endswith('Z') for instruction in current_instructions):
+while not all(instruction.endswith('Z') for instruction in current_instructions) and None in lcm:
     if instructions[current_index] == 'L':
         current_instructions = [map[instruction][0] for instruction in current_instructions]
     else:
@@ -20,4 +21,15 @@ while not all(instruction.endswith('Z') for instruction in current_instructions)
     moves += 1
     current_index = (current_index + 1) % len(instructions)
 
-print(moves)
+    for i, instruction in enumerate(current_instructions):
+        if instruction.endswith('Z') and i < len(lcm) and lcm[i] is None:
+            lcm[i] = moves
+            if len(lcm) == len(current_instructions):  # Check if lcm list is complete
+                break
+
+    # FILEPATH: /C:/Users/micro/Documents/Dev/adventofcode2023/day8_part2.py
+    
+lcm_value = math.lcm(*lcm)
+print(lcm_value)
+    
+    
